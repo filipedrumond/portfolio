0f1d90db15fe38006613fdd8b260a78a61111572
filipedrumond <filipe.dp@outlook.com>
2022-12-22 23:59:48 -0300

    ➕ version: v-1.0.6 Gerada automaticamente no push

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 
 app.use('/', express.static('dist'))
-app.listen(8090, () => {
-  console.log('running 8090')
+app.listen(80, () => {
+  console.log('running 80')
 })
