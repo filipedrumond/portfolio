8d12c08795b970b78dd4f8bba15a2e40001dd990
filipedrumond <filipe.dp@outlook.com>
2023-01-16 20:25:39 -0300

    ✨ feat: database

---
+++ db/server.db.js
@@ -0,0 +1,17 @@
+const dbServer = (port) => {
+  const path = require('path')
+
+  const jsonServer = require('json-server')
+  const server = jsonServer.create()
+  const router = jsonServer.router(path.join(__dirname, './db.json'))
+  const middlewares = jsonServer.defaults()
+
+  server.use(middlewares)
+  server.use(router)
+
+  // const port = 510
+  return server.listen(port, (_) => {
+    console.log('JSON Server is running on ', port)
+  })
+}
+dbServer(3001)
