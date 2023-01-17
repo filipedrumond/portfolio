27c281e2391e37f6874116d5965bee71575a64f1
filipedrumond <filipe.dp@outlook.com>
2023-01-16 21:13:32 -0300

    teste

--- server.db.js
+++ server.db.js
@@ -3,7 +3,7 @@ const dbServer = (port) => {
 
   const jsonServer = require('json-server')
   const server = jsonServer.create()
-  const router = jsonServer.router(path.join(__dirname, './db.json'))
+  const router = jsonServer.router(path.join(__dirname, './db/db.json'))
   const middlewares = jsonServer.defaults()
 
   server.use(middlewares)
