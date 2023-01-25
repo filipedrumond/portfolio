042ba5f7eb8056ae3c03cb97065bc8f458b2d9df
filipedrumond <filipe.dp@outlook.com>
2023-01-25 20:23:08 -0300

    🔥 build: deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const baseDir = `${__dirname}/../dist/`
+app.use(express.static(`${baseDir}`))
+app.get('*', (req, res) => res.sendfile('index.html', { root: baseDir }))
+app.get('/teste', (req, res) => {
+  res.send('Hello World!')
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log('running ' + port)
+})
